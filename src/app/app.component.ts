9ea9fe8c8e93a10913c83cecfe2fdd872b882480
smriti <iamsmritid@gmail.com>
2018-09-02 23:04:54 +0530

    added video capture

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,93 +1,91 @@
+
+// new code
 import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 declare var require: any;
-const RecordRTC = require('recordrtc/RecordRTC.min');
+let RecordRTC = require('recordrtc/RecordRTC.min');
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  getAudio = '';
+export class AppComponent implements AfterViewInit{
+
   private stream: MediaStream;
   private recordRTC: any;
-  public: HTMLAudioElement;
-
-  @ViewChild('audio') audio;
+  
+  @ViewChild('video') video;
 
   constructor() {
     // Do stuff
   }
 
-  // ngAfterViewInit() {
-  //   // set the initial state of the audio
-  //   const audio: HTMLaudioElement = this.audio.nativeElement;
-  //   audio.muted = false;
-  //   audio.controls = true;
-  //   audio.autoplay = false;
-  // }
+  ngAfterViewInit() {
+    // set the initial state of the video
+    let video:HTMLVideoElement = this.video.nativeElement;
+    video.muted = false;
+    video.controls = true;
+    video.autoplay = false;
+  }
 
-  // toggleControls() {
-  //   let audio: HTMLaudioElement = this.audio.nativeElement;
-  //   audio.muted = !audio.muted;
-  //   audio.controls = !audio.controls;
-  //   audio.autoplay = !audio.autoplay;
-  // }
+  toggleControls() {
+    let video: HTMLVideoElement = this.video.nativeElement;
+    video.muted = !video.muted;
+    video.controls = !video.controls;
+    video.autoplay = !video.autoplay;
+  }
 
-  // successCallback(stream: MediaStream) {
+  successCallback(stream: MediaStream) {
 
-  //   var options = {
-  //     mimeType: 'audio/webm', // or audio/webm\;codecs=h264 or audio/webm\;codecs=vp9
-  //     audioBitsPerSecond: 128000,
-  //     audioBitsPerSecond: 128000,
-  //     bitsPerSecond: 128000 // if this line is provided, skip above two
-  //   };
-  //   this.stream = stream;
-  //   this.recordRTC = RecordRTC(stream, options);
-  //   this.recordRTC.startRecording();
-  //   let audio: HTMLaudioElement = this.audio.nativeElement;
-  //   audio.src = window.URL.createObjectURL(stream);
-  //   this.toggleControls();
-  // }
+    var options = {
+      recorderType: RecordRTC.StereoAudioRecorder,
+      mimeType: 'video/wav'
+     }
+    this.stream = stream;
+    this.recordRTC = RecordRTC(stream, options);
+    this.recordRTC.startRecording();
+    let video: HTMLVideoElement = this.video.nativeElement;
+    video.src = window.URL.createObjectURL(stream);
+    this.toggleControls();
+  }
 
-  // errorCallback() {
-  //   //handle error here
-  // }
+  errorCallback() {
+    //handle error here
+  }
 
-  // processaudio(audioaudioWebMURL) {
-  //   let audio: HTMLaudioElement = this.audio.nativeElement;
-  //   let recordRTC = this.recordRTC;
-  //   audio.src = audioaudioWebMURL;
-  //   this.toggleControls();
-  //   var recordedBlob = recordRTC.getBlob();
-  //   recordRTC.getDataURL(function (dataURL) { });
-  // }
+  processVideo(audioVideoWebMURL) {
+    let video: HTMLVideoElement = this.video.nativeElement;
+    let recordRTC = this.recordRTC;
+    video.src = audioVideoWebMURL;
+    this.toggleControls();
+    var recordedBlob = recordRTC.getBlob();
+    recordRTC.getDataURL(function (dataURL) { });
+  }
 
-  // startRecording() {
-  //   let mediaConstraints = {
-  //     audio: {
-  //       mandatory: {
-  //         minWidth: 1280,
-  //         minHeight: 720
-  //       }
-  //     }, audio: true
-  //   };
-  //   navigator.mediaDevices
-  //     .getUserMedia(mediaConstraints)
-  //     .then(this.successCallback.bind(this), this.errorCallback.bind(this));
+  startRecording() {
+    let mediaConstraints = {
+      video: {
+        mandatory: {
+          minWidth: 1280,
+          minHeight: 720
+        }
+      }, audio: true
+    };
+    navigator.mediaDevices.getUserMedia({
+      audio: true,
+  })
+      .then(this.successCallback.bind(this), this.errorCallback.bind(this));
+}
 
+  stopRecording() {
+    let recordRTC = this.recordRTC;
+    recordRTC.stopRecording(this.processVideo.bind(this));
+    let stream = this.stream;
+    stream.getAudioTracks().forEach(track => track.stop());
+    stream.getVideoTracks().forEach(track => track.stop());
+  }
 
-  // }
-
-  // stopRecording() {
-  //   let recordRTC = this.recordRTC;
-  //   recordRTC.stopRecording(this.processaudio.bind(this));
-  //   let stream = this.stream;
-  //   stream.getAudioTracks().forEach(track => track.stop());
-  //   stream.getaudioTracks().forEach(track => track.stop());
-  // }
-
-  // download() {
-  //   this.recordRTC.save('audio.webm');
-  // }
+  download() {
+    this.recordRTC.save('video.webm');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, ApplicationRef } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
-
-
+import { FormsModule, FormBuilder} from '@angular/forms';
+import * as RecordRTC from 'recordrtc';
+import { RecordRtcComponent } from './record-rtc/record-rtc.component';
+import { removeNgStyles, createNewHosts } from '@angularclass/hmr';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecordRtcComponent
   ],
   imports: [
     BrowserModule,
-    AngularFontAwesomeModule
+    AngularFontAwesomeModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(public appRef: ApplicationRef) {}
+  hmrOnInit(store) {
+    console.log('HMR store', store);
+  }
+  hmrOnDestroy(store) {
+    let cmpLocation = this.appRef.components.map(cmp => cmp.location.nativeElement);
+    // recreate elements
+    store.disposeOldHosts = createNewHosts(cmpLocation);
+    // remove styles
+    removeNgStyles();
+  }
+  hmrAfterDestroy(store) {
+    // display new elements
+    store.disposeOldHosts();
+    delete store.disposeOldHosts;
+  }
+}
